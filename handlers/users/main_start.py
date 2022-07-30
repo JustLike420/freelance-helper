630d6db66da957e0ba52f4b5f3a6d608f3373229
Vladimir <justlikevova@gmail.com>
2022-07-30 22:52:28 +0300

    add buttons user's: about, keywords, search by keyw, fav, owner owner's: mailing, user search, stats

--- handlers/users/main_start.py
+++ handlers/users/main_start.py
@@ -9,12 +9,11 @@ db = SqlAlchemy()
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
     db.create_user(message.from_user.id, message.from_user.username)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ['Кнопк1', 'Кнопка2', 'Кнопка3']
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    buttons = ['О боте', 'Ключевые слова', 'Поиск по ключевым словам', 'Избранные', 'Автор']
 
     if str(message.from_user.id) in config.admins:
-        buttons.append('Кнопка4')
-        buttons.append('Кнопка5')
+        buttons.extend(['Рассылка', 'Поиск профиля', 'Статистика'])
     markup.add(*buttons)
 
     await message.answer(f"🤘 Салют, {message.chat.username}!\n", reply_markup=markup)
