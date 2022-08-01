bdf35464d06fc7905d1835de9186c0a437a793b1
Vladimir <justlikevova@gmail.com>
2022-08-01 21:06:58 +0300

    db: get/change keywords

--- utils/db/work_with_database.py
+++ utils/db/work_with_database.py
@@ -17,3 +17,17 @@ class SqlAlchemy:
                 user = User(user_id=user_id, username=username)
                 session.add(user)
                 session.commit()
+
+    def get_keywords(self, user_id):
+        with self.Session() as session:
+            user_keywords = session.query(User.keywords).filter_by(user_id=user_id).first()
+            if user_keywords == ('',):
+                return 'Пусто'
+            else:
+                return user_keywords[0]
+
+    def change_keywords(self, user_id, keywords):
+        with self.Session() as session:
+            user = session.query(User).filter_by(user_id=user_id).first()
+            user.keywords = keywords
+            session.commit()
