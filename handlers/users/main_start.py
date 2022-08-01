1852b9837315af60a17ed68628d36fc020fb99a6
Vladimir <justlikevova@gmail.com>
2022-08-01 21:11:53 +0300

    start command: add back to main, delete all states

--- handlers/users/main_start.py
+++ handlers/users/main_start.py
@@ -1,4 +1,6 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
+
 from keyboards import main_menu
 from loader import dp
 from utils.db.work_with_database import SqlAlchemy
@@ -6,8 +8,9 @@ from utils.db.work_with_database import SqlAlchemy
 db = SqlAlchemy()
 
 
-@dp.message_handler(commands='start')
-async def start(message: types.Message):
+@dp.message_handler(text=["⬅ На главную", "/start"], state="*")
+async def start(message: types.Message,state: FSMContext):
+    await state.finish()
     db.create_user(message.from_user.id, message.from_user.username)
     markup = main_menu(message.from_user.id)
     await message.answer(f"🤘 Салют, {message.chat.username}!\n", reply_markup=markup)
